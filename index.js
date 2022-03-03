6f50e62258f81b5aa972881aa0c49432a50c1605
Ruben André <rubandre14@gmail.com>
2022-03-03 18:08:08 +0100

    testeee

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express')
-const app = express()
-
+const app=express()
+const morgan=require('morgan')
+const bodyParser=require('body-parser')
+const cors = require('cors')
+const mongoose= require('mongoose')
+const dbuser='rubenAndre'
+const dbpass='l03p5D9gBLR2V4xk'
 const port = process.env.PORT || 3005
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 
 app.use('/', (req, res) => {
   return res.status(200).send({ message: 'ola seja bm vindo amigo' })
 })
-app.use('/produtos', (req, res) => {
-  return res.status(200).send({ message: 'um dia tu iras saber o quanto eu te amei' })
-})
-app.listen(port, () => {
-  console.info('aplicação rodando')
-})
+
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.t5c1s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`).then(()=>{
+app.listen(port)
+console.log('conectou ao banco🔥🔥🔥')
+}).catch((err)=>console.log(err))
