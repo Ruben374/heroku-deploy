fae087d1c26f3562c62c9d6bf2e85ebb9d35c65c
Ruben André <rubandre14@gmail.com>
2022-03-03 18:01:35 +0100

    mas um teste

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3005
 
 app.use('/', (req, res) => {
-  return res.status(200).send({ message: 'ola seja bm vindo' })
+  return res.status(200).send({ message: 'ola seja bm vindo amigo' })
 })
 app.use('/produtos', (req, res) => {
   return res.status(200).send({ message: 'um dia tu iras saber o quanto eu te amei' })
