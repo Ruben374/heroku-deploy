2157c0ee9b010d67981bb88bd605f76308fb2cb7
Ruben André <rubandre14@gmail.com>
2022-03-03 17:55:53 +0100

    teste rota produtos

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 3005
 app.use('/', (req, res) => {
   return res.status(200).send({ message: 'ola seja bm vindo' })
 })
-
+app.use('/produtos', (req, res) => {
+  return res.status(200).send({ message: 'um dia tu iras saber o quanto eu te amei' })
+})
 app.listen(port, () => {
   console.info('aplicação rodando')
 })
